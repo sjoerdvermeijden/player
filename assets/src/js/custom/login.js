56707a1f9a3ac97bf15e2c20b0baa7faabd036d3
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2020-05-30 15:54:24 +0200

    Adds some markup for the facebook login via JavaScript SDK

--- assets/src/js/custom/login.js
+++ assets/src/js/custom/login.js
@@ -2,15 +2,10 @@ window.addEventListener('DOMContentLoaded', (e) => {
     const body = document.body;
     const loginContainer = document.querySelector('.js-login');
 
-    // body.addEventListener('click', (e) => {
+    body.addEventListener('click', (e) => {    
+        if (!loginContainer.contains(e.target)) {
+            body.classList.remove('is-login');
+        }
+    })
 
-    //     if (body.classList.contains('is-login')) {
-
-            
-            
-    //         if (!loginContainer.contains(e.target)) {
-    //             body.classList.remove('is-login');
-    //         }
-    //     }
-    // })
 })
